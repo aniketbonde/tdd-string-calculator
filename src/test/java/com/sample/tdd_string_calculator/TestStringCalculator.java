27826064e8662cb59fa2b3c16b4581ee97e1fea1
Aniket Bonde <anikbonde@gmail.com>
2020-12-23 23:59:38 +0530

    Created new maven project.

---
+++ src/test/java/com/sample/tdd_string_calculator/TestStringCalculator.java
@@ -0,0 +1,17 @@
+package com.sample.tdd_string_calculator;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for String calculator
+ */
+public class TestStringCalculator 
+{
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
